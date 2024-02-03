f83ee914781de6416cff545580db1d19e552b0f3
Daryna Holikova <dgolikova0@gmail.com>
2024-02-03 00:44:38 +0000

    add animation

---
+++ src/js/scroll-btn.js
@@ -0,0 +1,24 @@
+const goTopBtn = document.querySelector('.go-top');
+
+window.addEventListener('scroll', trackScroll);
+
+goTopBtn.addEventListener('click', goTop);
+
+function trackScroll() {
+  const scrolled = window.pageYOffset;
+
+  const coords = document.documentElement.clientHeight;
+
+  if (scrolled > coords) {
+    goTopBtn.classList.add('go-top--show');
+  } else {
+    goTopBtn.classList.remove('go-top--show');
+  }
+}
+
+function goTop() {
+  if (window.pageYOffset > 1000) {
+    window.scrollBy(0, -300000);
+    setTimeout(goTop, 1000);
+  }
+}
